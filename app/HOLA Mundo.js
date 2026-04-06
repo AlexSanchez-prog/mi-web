25dbf38e66421d7ca90b437909e4f28ea038496e
unknown <alex.sanchez.carralero@gmail.com>
2026-04-07 01:18:36 +0200

    TEST 1.0

---
+++ app/HOLA Mundo.js
@@ -0,0 +1,8 @@
+﻿export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenido a mi web</h1>
+      <p>Esta es la página principal (/)</p>
+    </main>
+  );
+}
--- app/page2.js
+++
@@ -1,8 +0,0 @@
-﻿export default function Home() {
-  return (
-    <main>
-      <h1>Mi web</h1>
-      <p>Esta es la página principal</p>
-    </main>
-  );
-}
