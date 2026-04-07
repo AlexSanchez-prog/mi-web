89993efa484753bd11a1a650a62023031d0040f5
unknown <alex.sanchez.carralero@gmail.com>
2026-04-07 02:33:28 +0200

    TEST 1.0

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-﻿export default function Home() {
-    return (
-        <main>
-            <h1>¡Hola! Esta es mi nueva web</h1>
-            <p>Ya no aparece la página de inicio de Next.js.</p>
-        </main>
-    )
-}
